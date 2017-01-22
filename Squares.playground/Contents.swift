e1caeec29c26de20b02232d9088e74a7042a75b2
TJ Carney <tjcarney@TJs-MacBook-Air.local>
2017-01-22 15:20:28 -0500

    Completed Playground

--- Squares.playground/Contents.swift
+++ Squares.playground/Contents.swift
@@ -53,3 +53,5 @@ var square6 = Square2(topLeftCorner: (10.0, 10.0), lengthOfSide: 10.0)
 print("Area is 100.0? \(square6.area)")
 square6.area = 144.0
 print("Length of side is 12.0? \(square6.lengthOfSide)")
+
+//Completed
